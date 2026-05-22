84e7a47e35875903818e165bbb0319e5db69de9c
Alex <dok.go@yandex.ru>
2026-05-22 13:44:21 +0500

    Fix

--- src/js/core/htmlHelper.js
+++ src/js/core/htmlHelper.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
 function insertHtml() {
     const htmlConatiner = document.createElement('div');
     const html = `
-        <div class="dev-helper fixed flex bottom-0 right-2 bg-blue-200 px-2 py-1 rounded-t-lg cursor-pointer">test</div>
+        <div class="dev-helper fixed flex bottom-0 right-2 bg-blue-200 px-2 py-1 rounded-t-lg cursor-pointer">Dev helper</div>
     `;
     htmlConatiner.innerHTML = html;
     document.body.append(htmlConatiner);
